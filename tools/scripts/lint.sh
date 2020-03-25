bcf7ffa1c826b660b98dcfb785f3dd0957eab616
Ricardo Silva <ricardani@gmail.com>
2020-03-25 17:39:12 +0000

    Add Stryker to perform mutation testing

--- tools/scripts/lint.sh
+++ tools/scripts/lint.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 set -e
 printf "Running linting...\n"
+
 printf "Checking js and jsx files...\n"
 eslint "src/**/*.{js,jsx}"
+
 printf "Checking scss files...\n"
 stylelint "**/*.scss"
+
 printf "Lint passed!\n"
